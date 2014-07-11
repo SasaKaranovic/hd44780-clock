31d1a7c9ba026d7bb0340171f78bf67da92c731e
ColdKeyboard <coldkeyboard@users.noreply.github.com>
2014-07-11 19:26:52 +0200

    Separating Code PCB and etc

--- HD44780 Demo.X/build/default/production/lcd_hd44780_pic16.p1.d
+++
@@ -1,6 +0,0 @@
- build/default/production/lcd_hd44780_pic16.d  \
- build/default/production/lcd_hd44780_pic16.p1:  \
- lcd_hd44780_pic16.c  \
-lcd_hd44780_pic16.h  \
-myutils.h  \
-custom_char.h 
--- HD44780 Demo.X/build/default/production/main.p1.d
+++
@@ -1,9 +0,0 @@
- build/default/production/main.d  \
- build/default/production/main.p1:  \
- main.c  \
-ds1307.h  \
-main.h  \
-lcd_hd44780_pic16.h  \
-i2cbysk.h  \
-myutils.h  \
-custom_char.h 
