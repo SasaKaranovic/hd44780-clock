df6323c1eae9edc3ec5d12e196f15936bcd86e17
ColdKeyboard <coldkeyboard@users.noreply.github.com>
2015-07-05 03:57:37 +0200

    Removing old firmware

--- Microchip XC8 Source Files/build/default/production/i2cbysk.p1.d
+++
@@ -1,9 +0,0 @@
- build/default/production/i2cbysk.d  \
- build/default/production/i2cbysk.p1:  \
- i2cbysk.c  \
-ds1307.h  \
-main.h  \
-lcd_hd44780_pic16.h  \
-i2cbysk.h  \
-myutils.h  \
-custom_char.h 
